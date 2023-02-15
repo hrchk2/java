cd0194bebaf59b0d109e29e5c58b92b680298ae5
EC2 Default User <amrumineq@gmail.com>
2023-02-15 08:15:49 +0000

    fix

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,12 +1,25 @@
 public class FizzBuzz{
+
+  /**
+   * 1から順番に数を表示する
+   * その数が3で割り切れるなら"Fizz"、5で割り切れるなら"Buzz"、両方で割り切れるなら"FizzBuzz"と表示する
+   */
+
   public static void main(String[] args){
+    // 変数宣言
+    // ループカウンタ
     int i = 0;
+    // 繰り返し最大数
     int loopCount = 100;
+    // 3の倍数
     int numFizz = 3;
+    // 5の倍数
     int numBuzz = 5;
+    // Fizz文字列
     String strFizz = "Fizz";
+    // Buzz文字列
     String strBuzz = "Buzz";
-    
+
     for(i = 1;i <= loopCount; i++){
       if(i % (numFizz * numBuzz) == 0){
         System.out.println(strFizz + strBuzz);
@@ -17,9 +30,6 @@ public class FizzBuzz{
       }else {
         System.out.println(i);
       }
-      // if( i == loopCount){
-      //   System.out.println("ありがとうございました。");
-      // }
     }
   }
 }
--- kadai1.java
+++ kadai1.java
@@ -1,25 +1,25 @@
 public class kadai1{
   public static void main(String[] args){
-    //課題1-1 1から100までの整数をすべて足す
-    //繰り返し100まで 
+    //課題1-1 1から100までの整数を全て足す
+    //繰り返し100まで
     int loopCount = 100;
      //初期値
-    int num = 0;
-    
+    int sum = 0;
+
     for(int i= 1;i <= loopCount ; i++ ){
-      num += i;
+      sum += i;
     }
-    System.out.println(num);
-    
+    System.out.println("1から100までを全て足すと" + sum + "になります。");
+
     // 課題1-2 2から100までの偶数のみを足す
     //初期値
-    int evenNum = 0;
-    
+    int evenSum = 0;
+
     for(int i= 1; i <= loopCount ; i++){
       if(i % 2 == 0){
-        evenNum += i ;
+        evenSum += i ;
+      }
     }
-    }
-    System.out.println(evenNum);
+    System.out.println("2から100までの偶数を全て足すと" + evenSum + "になります。");
   }
 }
