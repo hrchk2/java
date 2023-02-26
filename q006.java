77369257b7aac35dad5753f09ac35c9485c5cfc6
EC2 Default User <amrumineq@gmail.com>
2023-02-26 07:39:19 +0000

    practice

---
+++ q006.java
@@ -0,0 +1,13 @@
+public class q006{
+  public static void main(String[] args){
+  
+  int loopCount = 10 ;
+  for(int i=1; i <= loopCount; i++){
+   if(i % 2 == 0){
+     System.out.print("◆");
+   }else{
+     System.out.print("◇"); 
+   }
+  }
+  }
+}
