a7bc47350893716eacdf9c6bd1f7f399ea8cfbe1
EC2 Default User <amrumineq@gmail.com>
2023-03-05 16:24:40 +0000

    practice

---
+++ q010.java
@@ -0,0 +1,13 @@
+public class q010{
+  public static void main(String[] args){
+  
+  int loop = 8; 
+  
+  for(int i=1;i <= loop;i++){
+    for(int j=0;j<i;j++){
+      System.out.print("■.");
+    }
+    System.out.println("a");
+  }
+  }
+}
