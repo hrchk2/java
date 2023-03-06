6af381afa544172070201e908f6e126bf05dfbd9
EC2 Default User <amrumineq@gmail.com>
2023-03-06 17:12:50 +0000

    practice

--- q009.java
+++ q009.java
@@ -21,9 +21,20 @@ public class q009{
           System.out.print("■");
         }
       }
-      
-    }
     System.out.println(); // 列ループが終われば改行
-  }
+    }
   }
 }
+    // 模範解答
+    // for (int i = 0; i < row; i++) {    // 行ループ
+    //   int x = i % 2; // 偶数行の場合は0、奇数行の場合は1
+      
+    //   for (int j = 0; j < col; j++) {    // 列ループ
+    //       // 偶数行の場合は偶数列が■、奇数行の場合は奇数列が■
+    //       if (j % 2 == x) {
+    //           System.out.print('■');
+    //       } else {
+    //           System.out.print('　'); // 全角スペースを表示
+    //       }
+    //   }
+    // }
