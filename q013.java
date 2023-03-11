ae35d6619e62c5771186565cdb3cd9a5eaf8fa4d
EC2 Default User <amrumineq@gmail.com>
2023-03-11 13:05:48 +0000

    practice

--- q013.java
+++ q013.java
@@ -1,11 +1,12 @@
-public class q012{
+public class q013{
   public static void main(String[] args){
   
   int loop = 8;  // 繰り返しの数
   
   for (int i = 0; i < loop; i++) {    // 行ループ
     for (int j = 0 ; j< loop; j++) {    // 列ループ
-      if(i == j){
+      int x = i + j; 
+      if(x == 7){
         System.out.print("■.");
         break; // breakを入れることでその時点でループが終了する。
       }else{
@@ -16,3 +17,6 @@ public class q012{
   }
   }
 }
+
+//模範解答
+// if文の中を(j == loop - i - 1)にする。
