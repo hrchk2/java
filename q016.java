1e11a224f62ef07b5b1621249e8d082b7149e0e1
EC2 Default User <amrumineq@gmail.com>
2023-03-15 12:05:52 +0000

    practice

---
+++ q016.java
@@ -0,0 +1,20 @@
+public class q016{
+  public static void main(String[] args){
+  
+  int loop = 7;
+  for(int i=1;i <=loop;i++){
+    int j = i * 5;
+    System.out.println(j);
+  }
+  
+  // 模範解答
+  // int n = 7;
+  // int bai = 5;
+  
+  // for(int i = 1;i<=7;i++){
+  //   int ans = i * bai;
+  //   System.out.println(ans);
+  // }
+  
+  }
+}
