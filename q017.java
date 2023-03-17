926077f2b3a10b735e563a1d9f66ec8f7d05be6c
EC2 Default User <amrumineq@gmail.com>
2023-03-17 12:13:54 +0000

    practice

---
+++ q017.java
@@ -0,0 +1,14 @@
+public class q017{
+  public static void main(String[] args){
+  
+  int max = 20;
+  int n = 4;
+  
+  for(int i=1;i <= max ;i++){
+    if(i % n == 0){
+      System.out.println(i);
+    }
+  }
+  
+  }
+}
