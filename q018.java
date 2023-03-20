763e170c56cace1cd2044b6f15da0ff412ce81fa
EC2 Default User <amrumineq@gmail.com>
2023-03-20 13:24:46 +0000

    practice

---
+++ q018.java
@@ -0,0 +1,16 @@
+public class q018{
+  public static void main(String[] args){
+  
+  int max = 10;
+  
+  for(int i=0;i < max ;i++){
+    int j = max - i;
+    System.out.println(j);
+  }
+  
+  //別回答
+  // for(int i = n;i >= 1 ;i--){
+  //   System.out.println(i);
+  // }
+  }
+}
