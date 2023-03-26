9cd26a49d00dd88361ba1fdaf5711faa264bb6ca
EC2 Default User <amrumineq@gmail.com>
2023-03-26 13:43:47 +0000

    practice

---
+++ q020.java
@@ -0,0 +1,13 @@
+public class q020{
+  public static void main(String[] args){
+  
+  int loop = 7;
+  for(int i = 1;i <= loop;i++){
+    if(i != 7){
+      System.out.print(i + ",");
+    }else{
+      System.out.print(i);
+    }
+  }
+}
+}
